ac10e9597c84233054636923fdca26b9d2ef3863
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-04-09 17:58:13 +0530

    sd

--- masters/urls.py
+++ masters/urls.py
@@ -38,6 +38,12 @@ urlpatterns = [
     path('delete-amenity/<amenity_id>', delete_amenity, name='delete_amenity'),  # create or fetch list of admins
     path('get-amenity/', get_amenity.as_view() , name='get_amenity '), 
 
+    path('add-customer-address/', add_customer_address, name='add_customer_address'),  # create or fetch list of admins
+    path('update-customer-address/<customer_address_id>', update_customer_address, name='update_customer_address'),  # create or fetch list of admins
+    path('list-customer-address/', list_customer_address, name='list_customer_address'),  # create or fetch list of admins
+    path('delete-customer-address/<customer_address_id>', delete_customer_address, name='delete_customer_address'),  # create or fetch list of admins
+    path('get-customer-address/', get_customer_address.as_view() , name='get_customer_address '), 
+
     path('add-service-category/', add_service_category, name='add_service_category'),  # create or fetch list of admins
     path('update-service-category/<service_category_id>', update_service_category, name='update_service_category'),  # create or fetch list of admins
     path('list-service-category/', list_service_category, name='list_service_category'),  # create or fetch list of admins
