ba53708f2a0b68204381c332deebfdaabd12a55f
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-04-09 14:44:53 +0530

    sd

--- masters/filters.py
+++ masters/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from .models import *
 
-class serviceFilter(django_filters.FilterSet):
+class EventFilter(django_filters.FilterSet):
     class Meta:
-        model = service
+        model = event
         exclude = ['image']  # ⛔ Exclude unsupported field
