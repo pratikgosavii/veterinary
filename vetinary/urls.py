1e2a6d61cf18670f6c7fb429a09f29a2e0c151f3
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-04-09 15:33:29 +0530

    sd

--- vetinary/urls.py
+++ vetinary/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import *
 
 urlpatterns = [
@@ -31,3 +35,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
