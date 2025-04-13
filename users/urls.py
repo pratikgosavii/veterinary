46fe0cc1f4b67c3d2b7cfefb5302b95caf1f8759
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-04-14 02:40:10 +0530

    sd

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login-admin/', login_admin, name='login_admin'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('update-user/', UserUpdateView.as_view(), name='UserUpdateView'),
+    path('get-user/', UsergetView.as_view(), name='UsergetView'),
     path('reset-password/', ResetPasswordView.as_view(), name='ResetPasswordView'),
     path('logout/', logout_page, name='logout'),
     
