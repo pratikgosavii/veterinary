be6d7d0b990c9a9f27aa35a4fb5d464a78dcfadd
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-04-14 15:40:54 +0530

    sd

--- daycare/serializers.py
+++ daycare/serializers.py
@@ -2,60 +2,58 @@ from rest_framework import serializers
 from users.models import User
 from django.contrib.auth.hashers import make_password
 from .models import *
+from masters.serializers import *
+
+
 
 class day_care_serializer(serializers.ModelSerializer):
-    email = serializers.CharField(source="user.email")
-    mobile = serializers.CharField(source="user.mobile")
-    password = serializers.CharField(write_only=True, required=False)
+    
+    
+    amenities = amenity_serializer(many=True, read_only=True)  # for GET
+    amenity_ids = serializers.PrimaryKeyRelatedField(  # for POST/PUT
+        many=True, queryset=amenity.objects.all(), write_only=True, required=False
+    )
+    email = serializers.CharField(source="user.email", read_only=True)
+    mobile = serializers.CharField(source="user.mobile", required=False)
 
     class Meta:
         model = day_care
         fields = [
             'id', 'user', 'name', 'images', 'location', 
             'description', 'price_per_hour', 'price_per_day', 
-            'amenities', 'rating', 'email', 'mobile', 'password'
+            'amenities', 'amenity_ids', 'rating', 'email', 'mobile'
         ]
         extra_kwargs = {
             'user': {'read_only': True},
-            'amenities': {'required': False},  # Optional field
         }
 
     def create(self, validated_data):
-        
-        print(validated_data)
+        request = self.context['request']
+        user = request.user
 
-        user_data = validated_data.pop('user', {})
-        email = user_data.get('email')
-        mobile = user_data.get('mobile')
-        password = validated_data.pop('password', None)
-        
-        # Check if user exists (optional, adjust logic as needed)
-      
-        # Create day_care instance linked to the user
-        user = User.objects.create(
-            mobile=mobile,
-            email=email,
-            is_daycare=True,
-            password=make_password(password)  # Encrypt password
-        )
+        if day_care.objects.filter(user=user).exists():
+            raise serializers.ValidationError("You have already registered a daycare.")
 
-        day_care_instance = day_care.objects.create(user=user, **validated_data)
-        return day_care_instance
+        amenities = validated_data.pop('amenity_ids', [])
+        mobile = validated_data.pop('user', {}).get('mobile', None)
+        if mobile:
+            user.mobile = mobile
+            user.save()
+
+        instance = day_care.objects.create(user=user, **validated_data)
+        if amenities:
+            instance.amenities.set(amenities)
+        return instance
 
     def update(self, instance, validated_data):
-        mobile = validated_data.pop('mobile', None)
-        password = validated_data.pop('password', None)
-
-        # Update user mobile/password if provided
+        amenities = validated_data.pop('amenity_ids', None)
+        mobile = validated_data.pop('user', {}).get('mobile', None)
         if mobile:
             instance.user.mobile = mobile
-        if password:
-            instance.user.password = make_password(password)
-        
-        instance.user.save()
+            instance.user.save()
+        if amenities is not None:
+            instance.amenities.set(amenities)
         return super().update(instance, validated_data)
-    
-
 
 
 class DayCareBookingSerializer(serializers.ModelSerializer):
--- daycare/urls.py
+++ daycare/urls.py
@@ -13,10 +13,9 @@ urlpatterns = [
     # path('update-day_care/<int:day_care_id>/', update_day_care, name='update_day_care'),
     # path('list-day_care/', list_day_care, name='list_day_care'),
     # path('delete-day_care/<int:day_care_id>/', delete_day_care, name='delete_day_care'),
-    path('get-day_care/', get_day_care.as_view(), name='get_day_care'),
+    path('get/', get_day_care.as_view(), name='get_day_care'),
 
-    path('login-day-care/', day_care_login.as_view(), name='login_day_care'),
-    path('signup-day-care/', day_care_signup.as_view(), name='signup_day_care'),
+    path('register/', day_care_register.as_view(), name='register_day_care'),
 
 
     path('book-daycare/', CreateDayCareBooking.as_view(), name='book_daycare'),
