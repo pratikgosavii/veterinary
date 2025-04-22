389c8f649d2ad6de4277b34b4324399249a6432b
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-04-22 12:13:39 +0530

    Sdsd

--- doctor/admin.py
+++ doctor/admin.py
@@ -8,3 +8,4 @@ from .models import *
 admin.site.register(doctor)
 admin.site.register(TestBookingReport)
 admin.site.register(OnlineConsultationAppointmentReport)
+admin.site.register(ConsultationAppointmentReport)
