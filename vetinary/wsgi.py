873d76b9644c6c38367c881f37f4689e023c0725
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-04-22 10:58:57 +0530

    Sdsd

--- vetinary/wsgi.py
+++ vetinary/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for vetinary project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 
-from django.core.wsgi import get_wsgi_application
+# Set Stream API credentials
+os.environ['STREAM_API_KEY'] = 'c7wwttj85hg7'
+os.environ['STREAM_API_SECRET'] = 'cgptvrzsttwj9vcebwy25k7y6aqkd6nxh56gkwxhsj3djs989g7k5wraprep926d'
 
+# Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vetinary.settings')
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
