0482a5f7242d9f197cfa2b88fcb09e7687f5b045
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-04-22 10:58:57 +0530

    Sdsd

--- vetinary/settings.py
+++ vetinary/settings.py
@@ -14,14 +14,9 @@ from pathlib import Path
 
 
 import os
-from pathlib import Path
-from dotenv import load_dotenv
-import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-load_dotenv(dotenv_path=os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
